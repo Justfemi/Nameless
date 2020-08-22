07aa9eaf04e3ae56f2943a440462730c904b98ac
Ajibade Olufemi <ajibadeolufemi94@gmail.com>
2020-08-22 21:02:03 +0100

    added mysql

--- myscrumy/myscrumy/settings.py
+++ myscrumy/myscrumy/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'myscrumy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myscrumy',
+        'USER': 'root',
+        'PASSWORD': 'ajibadeolufemi',
+        'HOST': 'localhost',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
