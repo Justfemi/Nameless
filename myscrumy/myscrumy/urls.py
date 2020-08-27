97688fdddfa53f78751f190a9494f6910ff41e5c
Ajibade Olufemi <ajibadeolufemi94@gmail.com>
2020-08-27 05:57:13 +0100

    urls-modified

--- myscrumy/myscrumy/urls.py
+++ myscrumy/myscrumy/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('ajibadeolufemi94scrumy/',include('ajibadeolufemi94scrumy.urls')),
+    path('ajibadeolufemi94scrumy/',include('ajibadeolufemi94scrumy.urls')),
 ]
