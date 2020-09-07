9de9990acbcdb83a2eeaa7ddf2b594aa99096006
Ajibade Olufemi <ajibadeolufemi94@gmail.com>
2020-09-07 02:09:51 +0100

    Lab8

--- myscrumy/ajibadeolufemi94scrumy/urls.py
+++ myscrumy/ajibadeolufemi94scrumy/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.get_grading_parameters),
+    path('', views.index, name="index"),
+    path('home', views.get_grading_parameters),
 ]
--- myscrumy/ajibadeolufemi94scrumy/views.py
+++ myscrumy/ajibadeolufemi94scrumy/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def get_grading_parameters(request):
+    return HttpResponse("Welcome to my Scrumy")
+
+def index(request):
     return HttpResponse("This is a Scrum Application")
     
 
