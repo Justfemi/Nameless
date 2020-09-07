860e982250d86a49fb3786c5504fbdb25685cf3d
Ajibade Olufemi <ajibadeolufemi94@gmail.com>
2020-09-07 14:56:55 +0100

    Models added

--- myscrumy/ajibadeolufemi94scrumy/models.py
+++ myscrumy/ajibadeolufemi94scrumy/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class GoalStatus( models.Model ):
+    status_name = models.CharField( max_length=20 )
+    def __str__( self ):
+        return self.status_name
+
+class ScrumyGoals( models.Model ):
+    goal_name = models.CharField( max_length=100 )
+    goal_id = models.IntegerField( default=20 )
+    created_by = models.CharField( max_length=30 )
+    moved_by = models.CharField( max_length=30 )
+    owner = models.CharField( max_length= 30 )
+    goal_status = models.ForeignKey( 
+        GoalStatus, 
+        on_delete = models.PROTECT, null=True )
+    user = models.ForeignKey(
+        User,
+        related_name='user',
+        on_delete = models.PROTECT )
+    def __str__( self ):
+        return self.goal_name
+
+class ScrumyHistory( models.Model ):
+    moved_by = models.CharField( max_length= 30 )
+    created_by = models.CharField( max_length= 30 )
+    moved_from = models.CharField( max_length= 30 )
+    moved_to = models.CharField( max_length= 30 )
+    time_of_action = models.DateTimeField( auto_now= False, auto_now_add= False)
+    def __str__( self ):
+        return self.created_by
+
