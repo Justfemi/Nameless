8f17c7be5a80e530572c7ebff5fc6a77d734d837
Ajibade Olufemi <ajibadeolufemi94@gmail.com>
2020-09-14 23:32:55 +0100

    added templates

--- myscrumy/ajibadeolufemi94scrumy/urls.py
+++ myscrumy/ajibadeolufemi94scrumy/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
 #   path('', views.get_grading_parameters),
     path('', views.index, name='index'),
     path('movegoal/<int:goal_id>', views.move_goal, name='move goal'),
+    path('addgoal', views.add_goal, name='add goal'),
+    path('home', views.home, name='home'),
+    path('accounts/', include('django.contrib.auth.urls') )
 ]
--- myscrumy/ajibadeolufemi94scrumy/views.py
+++ myscrumy/ajibadeolufemi94scrumy/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
-from .models import ScrumyGoals, ScrumyHistory, GoalStatus
-
+from .models import User, ScrumyGoals, ScrumyHistory, GoalStatus
+import random
 
 # Create your views here.
 #def get_grading_parameters(request):
@@ -22,6 +22,33 @@ def move_goal(request, goal_id):
         return render(request, 'exception.html', dictionary)
     return HttpResponse (goal.goal_name)
 
+def add_goal(request):
+    scrumy = ScrumyGoals.objects.create(goal_name = 'Keep Learning Django', goal_id= gen_id(), moved_by = 'Louis', 
+    created_by = 'Louis', owner = 'Louis', goal_status = GoalStatus.objects.get(status_name = 'Weekly Goals'), 
+    user = User.objects.get(username = 'louis')  )
+    scrumy.save()
+    return HttpResponse('Goal has been successgully added')
+    
+#def home (request):
+    #goals = ScrumyGoals.objects.filter(goal_name='Keep Learning Django')
+
+    #output = ', '.join([goal.goal_name for goal in goals])
+    #return HttpResponse( output )
+
+def gen_id():
+    id_list = ScrumyGoals.objects.all()
+    id_list = [idd.goal_id for idd in id_list]
+    g_id = random.randint(1000, 9999)
+    if g_id in id_list:
+        g_id = random.randint(1000, 9999)
+    else:
+        return g_id
+
+def home (request):
+    goals = ScrumyGoals.objects.filter(goal_name='Keep Learning Django')
+
+    output = ', '.join([goal.goal_name for goal in goals])
+    return HttpResponse( output )
 
     
 
