34b15086e7e9062dbc0392818987b1e1d0537156
Ajibade Olufemi <ajibadeolufemi94@gmail.com>
2020-09-14 14:50:17 +0100

    views modified

--- myscrumy/ajibadeolufemi94scrumy/urls.py
+++ myscrumy/ajibadeolufemi94scrumy/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-#    path('', views.get_grading_parameters),
+#   path('', views.get_grading_parameters),
     path('', views.index, name='index'),
+    path('movegoal/<int:goal_id>', views.move_goal, name='move goal'),
 ]
--- myscrumy/ajibadeolufemi94scrumy/views.py
+++ myscrumy/ajibadeolufemi94scrumy/views.py
@@ -11,6 +11,17 @@ def index(request):
     goal = ScrumyGoals.objects.filter(goal_name = 'Learn Django')
     return HttpResponse( goal )
 
+def move_goal(request, goal_id):
+
+    dictionary = {
+        'error': "Goal id doesn't exist"
+    }
+    try:
+        goal = ScrumyGoals.objects.get(goal_id = goal_id)
+    except:
+        return render(request, 'exception.html', dictionary)
+    return HttpResponse (goal.goal_name)
+
 
     
 
